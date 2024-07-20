fa75c012f7713a0925fd032e95094c77a720fda2
alex <sytnikov.zp@gmail.com>
2024-07-20 12:09:59 +0300

    Added sequelize to project

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+// const { Pool } = require('pg');
+// require('dotenv').config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+// });
 
-module.exports = pool;
+// module.exports = pool;
--- index.js
+++ index.js
@@ -2,15 +2,31 @@ const http = require('http');
 // ============================
 require('dotenv').config();
 // ============================
+const db = require('./src/db/models');
 const app = require('./src/app');
 // ============================
 
 // Create server with HTTP module
-const HOST_NAME = 'localhost';
+const HOST_NAME = process.env.DB_HOST;
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 
+const dbCheck = async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log(
+      `Connection with DB ${process.env.DB_NAME.toUpperCase()} has been successfully done!`
+    );
+  } catch (error) {
+    console.log(`Can't connect to DB: `, error.message);
+  }
+};
+
+dbCheck();
+
 server.listen(PORT, HOST_NAME, () =>
   console.log(`Server running at http://${HOST_NAME}:${PORT}`)
 );
+
+console.log('Server is started!');
